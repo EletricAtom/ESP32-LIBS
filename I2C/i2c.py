a64e66b4a6df3e4600979a8f542a7017c324dbc3
Josiel <107970605+EletricAtom@users.noreply.github.com>
2025-03-30 21:45:57 -0300

    Update i2c.py

--- I2C/i2c.py
+++ I2C/i2c.py
@@ -16,12 +16,12 @@ from time import sleep
 
 '''-----------------------------------------Inicio do Codigo-------------------------------------------------------------------------------'''
 i2c = SoftI2C(sda=Pin(21),scl=Pin(22),freq=9600) #Estamos iniciando o Protocolo i2c com sda no pino 21 e scl no pino 22 e baudrate de 9600
-dispositivosI2C =  i2c.scan() 					 #Estamos identificando os endereços dos dispositovos conectados ao barramento i2c
+dispositivosI2C =  i2c.scan() 					 #Estamos identificando os endereços dos dispositivos conectados ao barramento i2c
 
 print(dispositivosI2C)							 #Estamos imprimindo quais foram os dispositovos encontados no barramento I2C
 
 
-i2c.writeto_mem(80,2,b'\xFF')					 # Essa funçao de escrita pode ser encontrada no documentação do micropython ela é responsavel por fazer a escriat no endereço de memoria
+i2c.writeto_mem(80,2,b'\xFF')					 # Essa funçao de escrita pode ser encontrada no documentação do micropython ela é responsavel por fazer a escrita no endereço de memoria
 
 sleep(0.006)									 #Estava dando um erro desgraçado devido a não estar esperando o tempo de gravação
 a = i2c.readfrom_mem(80,2,1)					 #Após o dado gravado podemos efetuar a leitura
